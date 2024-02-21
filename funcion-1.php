f36a2919094c1cc170070c1c3f410a8ada50d137
Carlos Blockeado <45248351+Cuabu@users.noreply.github.com>
2024-02-21 15:58:34 -0500

    Insertando datos

---
+++ funcion-1.php
@@ -0,0 +1,12 @@
+<?php
+function mostrartexto ($texto)
+{
+    echo "<strong> El texto a mostrar es el siguiente: </strong>";
+    echo $texto;
+}
+?>
+
+<?php
+mostrartexto ("Me gusta mucho programar, soy aprendiz ADSI");
+?>
+
--- galletas2.php
+++ galletas2.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tabla de Multiplicar y Bucles en PHP con Bootstrap</title>
-    <!-- Incluir Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+
+
     <div class="container mt-5">
         <div class="row">
             <div class="col">
@@ -34,10 +35,12 @@
                 </table>
             </div>
         </div>
+
+
         <div class="row mt-5">
             <div class="col">
                 <h2>Bucle FOR decreciente de 50 a 40</h2>
-                <!-- Bucle FOR decreciente de 50 a 40 -->
+                <!-- Bucle FOR de 50 a 40 -->
                 <ul class="list-group">
                     <?php
                     for ($galletas = 50; $galletas >= 40; $galletas--) {
@@ -46,9 +49,11 @@
                     ?>
                 </ul>
             </div>
+
+
             <div class="col">
                 <h2>Bucle FOR que muestra el doble del valor de $contador</h2>
-                <!-- Bucle FOR que muestra el doble del valor de $contador -->
+                <!-- Bucle FOR que muestra el doble del valor de la variable contador -->
                 <ul class="list-group">
                     <?php
                     for ($contador = 1; $contador <= 5; $contador++) {
@@ -59,8 +64,6 @@
             </div>
         </div>
     </div>
-
-    <!-- Incluir Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
